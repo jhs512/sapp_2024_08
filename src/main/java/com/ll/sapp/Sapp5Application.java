016794df11944ab6816a0c78458869d2655131f4
jhs512 <jangka512@gmail.com>
2024-08-11 07:21:47 +0900

    work

---
+++ src/main/java/com/ll/sapp/Sapp5Application.java
@@ -0,0 +1,13 @@
+package com.ll.sapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sapp5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sapp5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sapp/Sapp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sapp5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
