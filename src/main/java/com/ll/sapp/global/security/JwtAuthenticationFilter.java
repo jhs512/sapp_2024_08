fbd43610cb0d1d3282839e48ad623b1abc0fc12b
jhs512 <jangka512@gmail.com>
2024-08-13 11:05:46 +0900

    work

--- src/main/java/com/ll/sapp/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ll/sapp/global/security/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.ll.sapp.global.security;
 
+import com.ll.sapp.member.AuthTokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,17 +16,43 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final AuthTokenService authTokenService;
+
     @SneakyThrows
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
         log.debug("JwtAuthenticationFilter request: {}", request.getRequestURI());
-        
-        User user = new User("user1", "", List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String accessToken = authorization.split("Bearer ")[1];
+        Map<String, Object> tokenData;
+
+        try {
+            tokenData = authTokenService.getDataFrom(accessToken);
+        } catch (Exception e) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String username = (String) tokenData.get("username");
+        List<String> authoritiesStringList = (List<String>) tokenData.get("authorities");
+        List<SimpleGrantedAuthority> authorities = authoritiesStringList.stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        User user = new User(username, "", authorities);
         Authentication authentication = new UsernamePasswordAuthenticationToken(user, "", user.getAuthorities());
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
